66f2d877eab3c2a928d3edb3bfc6f7209590d955
Alex H <48185123+Vodes@users.noreply.github.com>
2024-02-02 00:18:50 +0100

    Fix KtorExtension import

--- src/main/kotlin/moe/styx/downloader/torrent/flood/FloodClient.kt
+++ src/main/kotlin/moe/styx/downloader/torrent/flood/FloodClient.kt
@@ -7,10 +7,10 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.*
 import moe.styx.downloader.httpClient
-import moe.styx.downloader.setGenericJsonBody
 import moe.styx.downloader.torrent.Torrent
 import moe.styx.downloader.torrent.TorrentClient
 import moe.styx.downloader.utils.Log
+import moe.styx.downloader.utils.setGenericJsonBody
 import moe.styx.types.json
 import kotlin.random.Random
 
